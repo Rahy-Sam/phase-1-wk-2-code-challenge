db4bdd684b00b6f4a3bf92878f3caf63236822a5
Raymond <raymond.mwaiki@student.moringaschool.com>
2023-10-26 12:26:45 +0300

    updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Use fetch to make a request and retrieve data from the server
-fetch("http://localhost:3000/characters")
+fetch("https://my-json-server.typicode.com/Rahy-Sam/phase-1-wk-2-code-challenge/characters")
   .then((resp) => resp.json())
   .then((characters) => {
     console.log(characters);
@@ -24,6 +24,22 @@ function displayCharacterNames(characters) {
 
     ul.appendChild(liElement)
 
+    // This function enables the votes to add from the current input
+    // let characterVotes= document.getElementById('vote-count')
+    // characterVotes.textContent = candidate.votes;
+    // let currentVote= parseInt(characterVotes.textContent, 10)
+    // let votesForm= document.getElementById('votes-input')
+    // let votes = document.getElementById('votes-box')
+
+    // //Submit event for the votes
+    // votesForm.addEventListener('submit',(event)=>{
+    //   event.preventDefault()
+    //   // stops reset to default
+    //   let newVote = parseInt(votes.value, 10)
+    //   currentVote= currentVote + newVote
+    //   characterVotes.textContent= currentVote
+    // })
+
     // reset votes
     document.getElementById('btnResetVotes').addEventListener('click', () => {
       document.getElementById('character-votes').textContent = 0;
@@ -35,9 +51,10 @@ function addVotes() {
   document.getElementById('form-votes').addEventListener('submit', (event) => {
     event.preventDefault();
 
-    votesValue = document.getElementById('votes-input').value
-    document.getElementById('character-votes').textContent = votesValue
-
+    let votesValue = document.getElementById('votes-input').value
+    let charVotes = document.getElementById('character-votes').innerText 
+    charVotes = votesValue;
+console.log(charVotes)
     // clear input value after saving votes value in details
     document.getElementById('votes-input').value = ''
   })
